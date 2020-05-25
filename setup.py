a16e439773d4c580c0bf5d92b1f85f61a14db753
Marco A Rojas <marquicus@gmail.com>
2020-05-25 14:55:44 -0500

    [QUICKFIX] manifest+setup

--- setup.py
+++ setup.py
@@ -36,4 +36,5 @@ setup(
     package_data={},
     install_requires=['contextvars==2.*,>=2.4.0; python_version == "3.6.*" and python_version >= "3.6.0"', 'dephell==0.*,>=0.8.3', 'graphviz==0.*,>=0.13.2', 'jinja2==2.*,>=2.10.0'],
     extras_require={"dev": ["black==19.*,>=19.0.0", "isort==4.*,>=4.3.0", "pylint==2.*,>=2.4.0", "pytest==3.*,>=3.0.0", "rope==0.*,>=0.14.0"]},
+    include_package_data=True,
 )
